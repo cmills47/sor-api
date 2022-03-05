23cd6eb13cfbb979e283047317feebd9ad20a97a
Dana Epp <dana@vulscan.com>
2022-03-05 09:43:29 -0800

    Added new definitions for the Invite flow to OpenAPI, and scaffolded new Invite API to allow hosts and refugees to connect

--- api/InviteApi.cs
+++ api/InviteApi.cs
@@ -6,7 +6,6 @@
 #nullable disable
 
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -14,18 +13,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using SiteOfRefuge.API.Models;
 
 namespace SiteOfRefuge.API
 {
-    public class RefugeesApi
+    public class InviteApi
     {
-        private ILogger<RefugeesApi> _logger;
+        private ILogger<InviteApi> _logger;
 
-        /// <summary> Initializes a new instance of RefugeesApi. </summary>
+        /// <summary> Initializes a new instance of InviteApi. </summary>
         /// <param name="logger"> Class logger. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="logger"/> is null. </exception>
-        public RefugeesApi(ILogger<RefugeesApi> logger)
+        public InviteApi(ILogger<InviteApi> logger)
         {
             if (logger == null)
             {
@@ -35,89 +33,89 @@ namespace SiteOfRefuge.API
             _logger = logger;
         }
 
-        /// <summary> Get a summary list of refugees registered in the system. </summary>
+        /// <summary> Lists any current invitation requests for this user. </summary>
         /// <param name="req"> Raw HTTP Request. </param>
         /// <param name="cancellationToken"> The cancellation token provided on Function shutdown. </param>
-        [FunctionName("GetRefugeesAsync_get")]
-        public async Task<IActionResult> GetRefugeesAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "refugees")] HttpRequest req, CancellationToken cancellationToken = default)
+        [FunctionName("GetInvitesAsync_get")]
+        public async Task<IActionResult> GetInvitesAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "invite")] HttpRequest req, CancellationToken cancellationToken = default)
         {
             _logger.LogInformation("HTTP trigger function processed a request.");
 
             // TODO: Handle Documented Responses.
             // Spec Defines: HTTP 200
-
-            IEnumerable<RefugeeSummary> refugeeList = new List<RefugeeSummary> {
-                new RefugeeSummary( "3F2504E0-4F89-41D3-9A0C-0305E82C3301", "PL-06", 1, new DateTimeOffset(DateTime.UtcNow) ),
-                new RefugeeSummary( "2D2503E0-4D89-41C6-2D3E-1263EF2B1829", "PL-16", 4, new DateTimeOffset(DateTime.UtcNow) )
-            };
-            
-            return new OkObjectResult(refugeeList);
-
-        }
-
-        /// <summary> Registers a new refugee in the system. </summary>
-        /// <param name="body"> The Refugee to use. </param>
-        /// <param name="req"> Raw HTTP Request. </param>
-        /// <param name="cancellationToken"> The cancellation token provided on Function shutdown. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
-        [FunctionName("AddRefugeeAsync_post")]
-        public async Task<IActionResult> AddRefugeeAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "refugees")] Refugee body, HttpRequest req, CancellationToken cancellationToken = default)
-        {
-            _logger.LogInformation("HTTP trigger function processed a request.");
-
-            // TODO: Handle Documented Responses.
-            // Spec Defines: HTTP 201
-
-            throw new NotImplementedException();
-        }
-
-        /// <summary> Get information about a specific refugee. </summary>
-        /// <param name="req"> Raw HTTP Request. </param>
-        /// <param name="id"> Refugee id in UUID/GUID format. </param>
-        /// <param name="cancellationToken"> The cancellation token provided on Function shutdown. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
-        [FunctionName("GetRefugeeAsync_get")]
-        public async Task<IActionResult> GetRefugeeAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "refugees/{id}")] HttpRequest req, string id, CancellationToken cancellationToken = default)
-        {
-            _logger.LogInformation("HTTP trigger function processed a request.");
-
-            // TODO: Handle Documented Responses.
-            // Spec Defines: HTTP 200
+            // Spec Defines: HTTP 403
             // Spec Defines: HTTP 404
 
             throw new NotImplementedException();
         }
 
-        /// <summary> Updates a refugee in the system. </summary>
-        /// <param name="id"> Refugee id in UUID/GUID format. </param>
-        /// <param name="body"> The Refugee to use. </param>
+        /// <summary> Invite a refugee to connect. </summary>
+        /// <param name="body"> The Id to use. </param>
         /// <param name="req"> Raw HTTP Request. </param>
         /// <param name="cancellationToken"> The cancellation token provided on Function shutdown. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="id"/> or <paramref name="body"/> is null. </exception>
-        [FunctionName("UpdateRefugeeAsync_put")]
-        public async Task<IActionResult> UpdateRefugeeAsync(string id, [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "refugees/{id}")] Refugee body, HttpRequest req, CancellationToken cancellationToken = default)
+        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
+        [FunctionName("InviteRefugeeAsync_post")]
+        public async Task<IActionResult> InviteRefugeeAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "invite")] string body, HttpRequest req, CancellationToken cancellationToken = default)
         {
             _logger.LogInformation("HTTP trigger function processed a request.");
 
             // TODO: Handle Documented Responses.
             // Spec Defines: HTTP 204
+            // Spec Defines: HTTP 403
             // Spec Defines: HTTP 404
 
             throw new NotImplementedException();
         }
 
-        /// <summary> Schedules a refugee to be deleted from the system (after 7 days archival). </summary>
+        /// <summary> Show an invitation. </summary>
         /// <param name="req"> Raw HTTP Request. </param>
-        /// <param name="id"> Refugee id in UUID/GUID format. </param>
+        /// <param name="id"> Invite id in UUID/GUID format. </param>
         /// <param name="cancellationToken"> The cancellation token provided on Function shutdown. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
-        [FunctionName("DeleteRefugeeAsync_delete")]
-        public async Task<IActionResult> DeleteRefugeeAsync([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "refugees/{id}")] HttpRequest req, string id, CancellationToken cancellationToken = default)
+        [FunctionName("GetInviteAsync_get")]
+        public async Task<IActionResult> GetInviteAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "invite/{id}")] HttpRequest req, string id, CancellationToken cancellationToken = default)
         {
             _logger.LogInformation("HTTP trigger function processed a request.");
 
             // TODO: Handle Documented Responses.
-            // Spec Defines: HTTP 202
+            // Spec Defines: HTTP 200
+            // Spec Defines: HTTP 403
+            // Spec Defines: HTTP 404
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary> Accept an invitation to connect. </summary>
+        /// <param name="id"> Invite id in UUID/GUID format. </param>
+        /// <param name="body"> The Id to use. </param>
+        /// <param name="req"> Raw HTTP Request. </param>
+        /// <param name="cancellationToken"> The cancellation token provided on Function shutdown. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="id"/> or <paramref name="body"/> is null. </exception>
+        [FunctionName("AcceptInvitationAsync_put")]
+        public async Task<IActionResult> AcceptInvitationAsync(string id, [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "invite/{id}")] string body, HttpRequest req, CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation("HTTP trigger function processed a request.");
+
+            // TODO: Handle Documented Responses.
+            // Spec Defines: HTTP 204
+            // Spec Defines: HTTP 403
+            // Spec Defines: HTTP 404
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary> Withdraw invitation request. </summary>
+        /// <param name="req"> Raw HTTP Request. </param>
+        /// <param name="id"> Invite id in UUID/GUID format. </param>
+        /// <param name="cancellationToken"> The cancellation token provided on Function shutdown. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
+        [FunctionName("DeleteInviteAsync_delete")]
+        public async Task<IActionResult> DeleteInviteAsync([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "invite/{id}")] HttpRequest req, string id, CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation("HTTP trigger function processed a request.");
+
+            // TODO: Handle Documented Responses.
+            // Spec Defines: HTTP 200
             // Spec Defines: HTTP 404
 
             throw new NotImplementedException();
